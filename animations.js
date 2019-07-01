d4058f39e97a8bd2841b6bcf81138b8ee17c2a85
Vincent Simone <vincent.simone@bts.com>
2019-07-01 12:12:49 -0700

    created basic functionality

--- animations.js
+++ animations.js
@@ -1,5 +1,47 @@
+let KEY = 'testingtesting123';
+let HASH = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789{|}[]^_:;<=&>?@'.split('');
+let SHIFT = 24;
+let MAX = 30;
+
+function printPassword(password) {
+    $('main').append(`
+        <p>Your password is ${password}</p>
+    `)
+}
+
+function shiftLetter(letter) {
+    shiftedIndex = HASH.indexOf(letter) + SHIFT;
+    if (shiftedIndex > HASH.length) {
+        shiftedIndex = shiftedIndex - HASH.length;
+    }
+    return HASH[shiftedIndex];
+}
+
+function encrypt(keyword) {
+    let keywordArray = keyword.split('');
+    for(i=0; i<keywordArray.length; i++) {
+        keywordArray[i] = shiftLetter(keywordArray[i]);
+    }
+    return keywordArray.join('');
+}
+
+function generatePassword(keyword, maxLength) {
+    let startingPosition = keyword.length;
+    let keywordArray = keyword.split('');
+
+    for (i=startingPosition; i<maxLength; i++) {
+        keywordArray.unshift(
+            shiftLetter(keywordArray[0])
+        );
+    }
+    console.log(keywordArray.length);
+    return keywordArray.join('');
+}
+
 function loadPage() {
-    //do something
+    let encryptedKey = encrypt(KEY);
+    let pass = generatePassword(encryptedKey, MAX);
+    printPassword(pass);
 }
 
 $(loadPage);
